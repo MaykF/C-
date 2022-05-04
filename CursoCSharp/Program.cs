9df2968f9f8817e0bced4d3038723e5705d93ce1
Maycon Ferrari Varella <mayconfvarella@hotmail.com>
2022-05-03 23:16:36 -0300

    Treino

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string name;
+int year, age;
+
+Console.WriteLine("Ola seja bem-vindo!");
+
+Console.WriteLine("Para começar, digite seu nome:");
+
+name = Console.ReadLine();
+
+Console.WriteLine($"Ola, {name}! \nAgora digite o seu ano de nascimento:");
+
+year = int.Parse(Console.ReadLine());
+
+age = 2022 - year;
+
+Console.WriteLine($"Você tem {age} anos.");
